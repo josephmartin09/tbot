c9f4b0bb62aa58326057b689c9bd820dac84760d
Joseph Martin <josephmartin09@gmail.com>
2024-06-19 10:11:43 -0500

    Saving sym mgr progress

--- tbot/app/symbol_manager.py
+++ tbot/app/symbol_manager.py
@@ -1,39 +1,53 @@
-from collections.abc import MutableMapping
-
 # For now, we'll use threads, but we want this to be multi-process and/or ZMQ
 from threading import Lock
 
 
-class SymbolManager(MutableMapping):
+class SymbolManager:
     """Class to hold data for symbols that will be updated during the lifetime of an application."""
 
     def __init__(self):
         """Initialize the symbol manager."""
         super().__init__()
-        self._store = {}
+        self._symbols = {}
         self._lock = Lock()
 
-    def __delitem__(self, key):
-        """Delete an item from the symbol manager."""
-        with self._lock:
-            del self._store[key]
+    def add_symbol(self, name):
+        """Register a symbol.
 
-    def __getitem__(self, key):
-        """Get an item from the symbol manager."""
-        with self._lock:
-            return self._store[key]
+        :param str name: The name of the symbol
+        """
+        pass
 
-    def __setitem__(self, key, value):
-        """Update an item from the symbol manager."""
-        with self._lock:
-            self._store[key] = value
+    def add_feed(self, symbol, name, initial_feed_data):
+        """Register a feed to a symbol.
 
-    def __iter__(self):
-        """Return an iterator for the symbol manager."""
-        with self._lock:
-            return iter(self._store)
+        :param str symbol: The name of the symbol to add the feed to
+        :param str name: The name of the feed
+        :param object initial_feed_data: The initial data of the feed
+        """
+        pass
 
-    def __len__(self):
-        """Return the total number of keys in the symbol manager."""
-        with self._lock:
-            return len(self._store)
+    def update_feed(self, symbol, feed_name, update):
+        """Update a feed.
+
+        :param str symbol: The name of the symbol
+        :param str feed: The name of the feed
+        :param object update: The update to add to the feed
+        """
+        if symbol not in self._symbols:
+            raise ValueError(f"Symbol {symbol} is not registered")
+
+        feeds = self._symbols[symbol]
+        if feed_name not in feeds:
+            raise ValueError(f"Feed {feed_name} is not registered")
+
+        feed = feeds[feed_name]
+        feed.append(update)
+
+    def remove_feed(self, symbol, feed):
+        """Remove a feed from the symbol manager.
+
+        :param str symbol: The name of the symbol that contains the feed.
+        :param str feed: The name of the feed to remove.
+        """
+        pass
